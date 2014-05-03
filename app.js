51dece3694cfeb7b78d9e612f2a22ea99588216d
Ricardo Havill <rhavill2002@yahoo.com>
2014-05-03 17:09:27 -0400

    Store data sent to bot in variables.

--- app.js
+++ app.js
@@ -3,10 +3,25 @@ var express = require('express');
 var app = express();
 app.use(express.bodyParser());
 app.post('/poker-bot', function(req, res){
-  // possible actions: raise, bet, fold, call, check and allin
-  var actionsAllowed = req.param('actions').split("\n");
-  console.log(actionsAllowed+"\n\n");
-  res.send(actionsAllowed[actionsAllowed.length-1]);
+	var playerName = req.param('name');
+	// possible actions: raise, bet, fold, call, check and allin
+	var actionsAllowed = req.param('actions').split("\n");
+	var pocket = req.param('pocket').split(' ');
+	var hand = [
+		{ rank: pocket[0].split('')[0], suit: pocket[0].split('')[1] },
+		{ rank: pocket[1].split('')[0], suit: pocket[1].split('')[1] }
+	];
+  	var gameState = req.param('state');
+
+	var stateObject = xml.parseString(gameState);
+	var objects = {};
+	for (var i=0; i < stateObject.childs.length; i++) {
+		objects[stateObject.childs[i].name] = stateObject.childs[i];
+	}
+	// for (var i=0; i < objects['table'].length; i++) {
+	// }
+	console.log(objects.table.attrib);
+	res.send(actionsAllowed[actionsAllowed.length-1]);
 });
 
 var server = app.listen(3000, function() {
