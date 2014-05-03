215faf2f213d7cef4fab0953d696e3546c642b1a
Ricardo Havill <rhavill2002@yahoo.com>
2014-05-03 12:08:37 -0400

    Created simple player that always takes the last action in the list of allowed actions. This appears to result in a player that plays every hand to the end.

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+var xml = require('node-xml-lite');
 var express = require('express');
 var app = express();
 app.use(express.bodyParser());
 app.post('/poker-bot', function(req, res){
-  res.send(req.param('name'));
+  // possible actions: raise, bet, fold, call, check and allin
+  var actionsAllowed = req.param('actions').split("\n");
+  console.log(actionsAllowed+"\n\n");
+  res.send(actionsAllowed[actionsAllowed.length-1]);
 });
 
 var server = app.listen(3000, function() {
